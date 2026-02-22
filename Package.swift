e25a2c054cbda2d3f3985e7c84ddfbfe327a2e86
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-22 20:21:20 +0800

    fix: pin swift-argument-parser to 1.3-1.4 for Xcode 15.2 compat

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v14)
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", "1.3.0"..<"1.5.0")
     ],
     targets: [
         .target(
