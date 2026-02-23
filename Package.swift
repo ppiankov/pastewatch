932e4a1f89a32318e2aabf9af9084d552f31be3e
ppiankov <103106369+ppiankov@users.noreply.github.com>
2026-02-23 12:49:31 +0800

    feat: add Linux binary support

--- Package.swift
+++ Package.swift
@@ -2,39 +2,50 @@
 
 import PackageDescription
 
+var targets: [Target] = [
+    .target(
+        name: "PastewatchCore",
+        dependencies: [
+            .product(name: "Crypto", package: "swift-crypto", condition: .when(platforms: [.linux]))
+        ],
+        path: "Sources/PastewatchCore"
+    ),
+    .executableTarget(
+        name: "PastewatchCLI",
+        dependencies: [
+            "PastewatchCore",
+            .product(name: "ArgumentParser", package: "swift-argument-parser")
+        ],
+        path: "Sources/PastewatchCLI"
+    ),
+    .testTarget(
+        name: "PastewatchTests",
+        dependencies: ["PastewatchCore"],
+        path: "Tests/PastewatchTests"
+    )
+]
+
+#if os(macOS)
+targets.append(
+    .executableTarget(
+        name: "Pastewatch",
+        dependencies: ["PastewatchCore"],
+        path: "Sources/Pastewatch",
+        resources: [
+            .copy("Resources/AppIcon.icns")
+        ]
+    )
+)
+#endif
+
 let package = Package(
     name: "Pastewatch",
     platforms: [
         .macOS(.v14)
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", "1.3.0"..<"1.5.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", "1.3.0"..<"1.5.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "3.0.0"..<"4.0.0")
     ],
-    targets: [
-        .target(
-            name: "PastewatchCore",
-            path: "Sources/PastewatchCore"
-        ),
-        .executableTarget(
-            name: "Pastewatch",
-            dependencies: ["PastewatchCore"],
-            path: "Sources/Pastewatch",
-            resources: [
-                .copy("Resources/AppIcon.icns")
-            ]
-        ),
-        .executableTarget(
-            name: "PastewatchCLI",
-            dependencies: [
-                "PastewatchCore",
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ],
-            path: "Sources/PastewatchCLI"
-        ),
-        .testTarget(
-            name: "PastewatchTests",
-            dependencies: ["PastewatchCore"],
-            path: "Tests/PastewatchTests"
-        )
-    ]
+    targets: targets
 )
