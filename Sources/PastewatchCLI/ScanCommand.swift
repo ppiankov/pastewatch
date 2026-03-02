945c8692b0aa4712e0cbecfcea4a04f9fab8dd48
Test <test@example.com>
2026-03-02 20:07:12 +0800

    chore: bump version to 0.17.4

--- Sources/PastewatchCLI/ScanCommand.swift
+++ Sources/PastewatchCLI/ScanCommand.swift
@@ -371,7 +371,7 @@ struct Scan: ParsableCommand {
             }
         case .sarif:
             let pairs = results.map { ($0.filePath, $0.matches) }
-            let data = SarifFormatter.formatMultiFile(fileResults: pairs, version: "0.17.3")
+            let data = SarifFormatter.formatMultiFile(fileResults: pairs, version: "0.17.4")
             print(String(data: data, encoding: .utf8)!)
         case .markdown:
             print(MarkdownFormatter.formatDirectory(results: results), terminator: "")
@@ -402,7 +402,7 @@ struct Scan: ParsableCommand {
             }
         case .sarif:
             let pairs = results.map { ($0.filePath, $0.matches) }
-            let data = SarifFormatter.formatMultiFile(fileResults: pairs, version: "0.17.3")
+            let data = SarifFormatter.formatMultiFile(fileResults: pairs, version: "0.17.4")
             print(String(data: data, encoding: .utf8)!)
         case .markdown:
             print(MarkdownFormatter.formatDirectory(results: results), terminator: "")
@@ -432,7 +432,7 @@ struct Scan: ParsableCommand {
             }
         case .sarif:
             let data = SarifFormatter.format(
-                matches: matches, filePath: filePath, version: "0.17.3"
+                matches: matches, filePath: filePath, version: "0.17.4"
             )
             print(String(data: data, encoding: .utf8)!)
         case .markdown:
@@ -457,7 +457,7 @@ struct Scan: ParsableCommand {
             }
         case .sarif:
             let data = SarifFormatter.format(
-                matches: matches, filePath: filePath, version: "0.17.3"
+                matches: matches, filePath: filePath, version: "0.17.4"
             )
             print(String(data: data, encoding: .utf8)!)
         case .markdown:
